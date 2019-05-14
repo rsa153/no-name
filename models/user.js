8055d2406031b4d667b8269787d9a6c7ba49267c
rsa153 <r-saber@nlaw.northwestern.edu>
2019-05-13 19:48:32 -0500

    password encryption worksgit add .

--- models/user.js
+++ models/user.js
@@ -1,16 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt-nodejs');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  cpassword: { type: String, required: true },
   tasks: { type: [String] }
 });
 
 // Define schema methods
+userSchema.pre("save", function(next) {
+	var user = this;
+	bcrypt.genSalt(saltRounds, function(err, salt) {
+		bcrypt.hash(user.password, salt, function(err, hash) {
+			console.log(user.password)
+			user.password = hash;
+			next();
+		});
+	});
+});
+
 userSchema.methods = {
 	checkPassword: function (inputPassword) {
 		return bcrypt.compareSync(inputPassword, this.password)
